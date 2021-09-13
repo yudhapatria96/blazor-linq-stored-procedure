6313b289ea3193a7ff28660903ab7aabf6546c08
Yudha Patria <yudha.patria@mitra10.com>
2021-09-13 07:49:57 +0700

    first

---
+++ LinqToSql/SampleDefault.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="SampleDefault.aspx.cs" Inherits="SampleDefault" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+            <asp:Label ID="lblDept" runat="server"></asp:Label>
+        &nbsp;<br />
+        </div>
+        <asp:Button ID="btnGetData" runat="server" OnClick="btnGetData_Click" Text="Get Data" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnInsert" runat="server" OnClick="btnInsert_Click" Text="Insert" />
+&nbsp;&nbsp;
+        <asp:Button ID="btnUpdate" runat="server" OnClick="btnUpdate_Click" Text="Update" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="BtnDelete" runat="server" OnClick="BtnDelete_Click" Text="Delete" />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnGetEmployeesByDeparment" runat="server" OnClick="btnGetEmployeesByDeparment_Click" Text="Get Employees By Departement" />
+    </form>
+</body>
+</html>
---
+++ LinqToSql/SampleDefault.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SampleDefault : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void GetData()
+    {
+        SampleDataClassesDataContext dbContext = new SampleDataClassesDataContext();
+
+        //var linQuery = from employee in dbContext.Employees select employee;
+
+        //string sqlQuery = linQuery.ToString();
+        //Response.Write(sqlQuery);
+        //Response.Write(dbContext.GetCommand(linQuery).CommandText);
+
+        GridView1.DataSource = dbContext.GetEmployees();
+        GridView1.DataBind();
+    }
+
+    protected void btnGetData_Click(object sender, EventArgs e)
+    {
+        GetData();
+    }
+
+    protected void btnInsert_Click(object sender, EventArgs e)
+    {
+        using (SampleDataClassesDataContext dbContext = new SampleDataClassesDataContext())
+        {
+            Employee newEmployee = new Employee
+            {
+                FirstName = "Tim",
+                LastName = "Tum",
+                Gender = "Male",
+                Salary = 55000,
+                DepartmentId = 1
+            };
+
+            dbContext.Employees.InsertOnSubmit(newEmployee);
+            dbContext.SubmitChanges();
+        }
+        GetData();
+    }
+
+    protected void btnUpdate_Click(object sender, EventArgs e)
+    {
+
+        using (SampleDataClassesDataContext dbContext = new SampleDataClassesDataContext())
+        {
+            Employee employee = dbContext.Employees.SingleOrDefault(x => x.ID == 12);
+            employee.Salary = 6500;
+            dbContext.SubmitChanges();
+        }
+        GetData();
+    }
+
+    protected void BtnDelete_Click(object sender, EventArgs e)
+    {
+        using (SampleDataClassesDataContext dbContext = new SampleDataClassesDataContext())
+        {
+            Employee employee = dbContext.Employees.SingleOrDefault(x => x.ID == 12);
+            dbContext.Employees.DeleteOnSubmit(employee);
+            dbContext.SubmitChanges();
+        }
+        GetData();
+    }
+
+    protected void btnGetEmployeesByDeparment_Click(object sender, EventArgs e)
+    {
+        using(SampleDataClassesDataContext dbContext = new SampleDataClassesDataContext())
+        {
+            string deptName = string.Empty;
+            GridView1.DataSource = dbContext.GetEmployeesByDepartment(1, ref deptName);
+            GridView1.DataBind();
+
+            lblDept.Text = "Depart Name = " + deptName;
+        }
+    }
+}
